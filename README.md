273142e3c1ace43f03238022f8675ee5a0b78655
2106055MuhammadGofur <2106055@itg.ac.id>
2024-07-03 10:49:46 +0700

    first commit gofur

---
+++ README.md
@@ -0,0 +1,27 @@
+# Converter Mata Uang
+
+Kelompok 1:
+- Muhammad Gofur 210605 ( fitur utama )
+- Rama Mohammad Amirulhaq 2106048 ( penambahan metod delete )
+- M.Fikri Hermansyah 2106096 ( penambahan metod swap )
+- Raditia Pratika Ramadhan 2106097 ( penambahan metod get )
+
+Aplikasi Converter Mata Uang adalah aplikasi yang memanfaatkan layanan API (Application Programming Interface) dari penyedia data kurs mata uang untuk melakukan konversi nilai tukar antar mata uang secara otomatis dan real-time.
+
+hasil:
+
+<img width="212" alt="1" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/f4a33585-6c15-422e-bfd2-30e2d28926bc">
+<img width="211" alt="2" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/b6b51653-02b3-4f85-8657-e2bd90170d14">
+<img width="211" alt="3" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/e5292870-ad5c-4a60-9ba2-d0314b7add1a">
+<img width="211" alt="4" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/03fd25eb-9be0-4fc0-833f-13d78031d8ca">
+<img width="211" alt="5" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/a5544a9d-25b8-4b30-a70f-813ae35f643f">
+
+darkmode:
+
+<img width="211" alt="6" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/ebd70d51-265e-41ab-82ff-d238660489b7">
+<img width="211" alt="7" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/2e2e2110-e2fe-48f8-8b8a-b30b21578335">
+<img width="211" alt="8" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/6a8f2bda-0d95-4083-97bb-3fb8bff03447">
+<img width="211" alt="9" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/d3906209-66a4-4fb4-8f0b-b05fbab0b86d">
+<img width="209" alt="10" src="https://github.com/2106055MuhammadGofur/TB-PRAKTIKUM-MOBILE/assets/127308482/587cff29-459f-4d44-99c9-67b3a427c149">
+
+
---
+++ lib/home_screen.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String fromCurrency = "IDR";
+  String toCurrency = "USD";
+  double rate = 0.0;
+  double total = 0.0;
+  TextEditingController amountController = TextEditingController();
+  List<String> currencies = [];
+  bool isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrencies();
+  }
+
+  Future<void> _getCurrencies() async {
+    var response = await http
+        .get(Uri.parse('https://api.exchangerate-api.com/v4/latest/IDR'));
+
+    var data = jsonDecode(response.body);
+    setState(() {
+      currencies = (data['rates'] as Map<String, dynamic>).keys.toList();
+      rate = data['rates'][toCurrency];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      appBar: AppBar(
+        backgroundColor: isDarkMode ? Colors.transparent : Colors.white,
+        elevation: 0,
+        foregroundColor: isDarkMode ? Colors.white : Colors.black,
+        title: Text("Converter Mata Uang",
+            style: TextStyle(color: isDarkMode ? Colors.white : Colors.black)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(isDarkMode ? Icons.nightlight_round : Icons.wb_sunny),
+            onPressed: () {
+              setState(() {
+                isDarkMode = !isDarkMode;
+              });
+            },
+            color: isDarkMode ? Colors.white : Colors.black,
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(40),
+                child: Image.asset(
+                  'images/exchange.png',
+                  width: MediaQuery.of(context).size.width / 3,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                child: TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                      color: isDarkMode ? Colors.white : Colors.black),
+                  decoration: InputDecoration(
+                    labelText: "Jumlah",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    labelStyle: TextStyle(
+                        color: isDarkMode ? Colors.white : Colors.black),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                          color: isDarkMode ? Colors.white : Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(
+                            color: isDarkMode ? Colors.white : Colors.black)),
+                  ),
+                  onChanged: (value) {
+                    if (value != '') {
+                      setState(() {
+                        double amount = double.parse(value);
+                        total = amount * rate;
+                      });
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      child: DropdownButton<String>(
+                        value: fromCurrency,
+                        isExpanded: true,
+                        style: TextStyle(
+                            color: isDarkMode ? Colors.white : Colors.black),
+                        dropdownColor: isDarkMode ? Colors.black : Colors.white,
+                        underline: Container(
+                          height: 2,
+                          color: isDarkMode
+                              ? Colors.white
+                              : Color.fromARGB(255, 155, 155, 155),
+                        ),
+                        items: currencies.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(value),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      icon:
+                                          Icon(Icons.delete, color: Colors.red),
+                                      onPressed: () => _deleteCurrency(value),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            fromCurrency = newValue!;
+                            _getRate();
+                          });
+                        },
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: _swapCurrencies,
+                        icon: Icon(
+                          Icons.swap_horiz,
+                          size: 40,
+                          color: isDarkMode ? Colors.white : Colors.black,
+                        )),
+                    SizedBox(
+                      width: 100,
+                      child: DropdownButton<String>(
+                        value: toCurrency,
+                        isExpanded: true,
+                        style: TextStyle(
+                            color: isDarkMode ? Colors.white : Colors.black),
+                        dropdownColor: isDarkMode ? Colors.black : Colors.white,
+                        underline: Container(
+                          height: 2,
+                          color: isDarkMode
+                              ? Colors.white
+                              : Color.fromARGB(255, 155, 155, 155),
+                        ),
+                        items: currencies.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(value),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      icon:
+                                          Icon(Icons.delete, color: Colors.red),
+                                      onPressed: () => _deleteCurrency(value),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            toCurrency = newValue!;
+                            _getRate();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Harga: $rate",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: isDarkMode ? Colors.white : Colors.black,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '${total.toStringAsFixed(3)}',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: isDarkMode
+                      ? Color.fromARGB(255, 63, 223, 45)
+                      : Color.fromARGB(255, 19, 185, 19),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
